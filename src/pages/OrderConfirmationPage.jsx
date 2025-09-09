861d8634b259b91e8b7f4a54477114ed1a55effc
SONG NAKJOO <nakjoo@NJ-Mac-mini.local>
2025-09-09 16:25:58 +0900

    줄바꿈

--- src/pages/OrderConfirmationPage.jsx
+++ src/pages/OrderConfirmationPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OrderConfirmationPage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center">
-      <h1 className="text-lg md:text-2xl font-bold mb-4">감사합니다. 신청이 정상적으로 접수되었습니다.</h1>
+      <h1 className="text-lg md:text-2xl font-bold mb-4">감사합니다.<br/> 신청이 정상적으로 접수되었습니다.</h1>
       <p>확인 후 연락드리겠습니다.</p>
     </div>
   );
